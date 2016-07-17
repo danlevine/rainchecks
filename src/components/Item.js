cc23bc0a96ecd222046648f114023b98d9dd03c2
Daniel Levine <dlevine210@gmail.com>
2016-07-17 00:52:18 -0500

    Add ES6 string interpolation in template

--- src/components/Item.js
+++ src/components/Item.js
@@ -19,12 +19,12 @@ const Item = ({
   scoreTomatoUser,
   tomatoConsensus }) =>
     <li className="item">
-      <img className="item__poster" src={poster} alt={name + " movie poster"} />
+      <img className="item__poster" src={poster} alt={`${name} movie poster`} />
       <div className="item__info">
         <div>
           <h2 className="item__info__name">{name}</h2>
           <p className="item__info__genre">{genre}</p>
-          <p className="item__info__tech">{year + ' ' + rated + ' ' + runtime}</p>
+          <p className="item__info__tech">{`${year} ${rated} ${runtime}`}</p>
         </div>
         <ul className="item__info__scores">
           <li>{scoreTomato}%</li>
