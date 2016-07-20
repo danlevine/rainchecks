971ef80dd7fb1435a1ae1a310b2f72f37ed7f74e
Daniel Levine <dlevine210@gmail.com>
2016-07-20 14:42:09 -0500

    Small breakpoint layout/styling CP

--- src/components/Item.js
+++ src/components/Item.js
@@ -20,24 +20,27 @@ const Item = ({
   tomatoConsensus }) =>
     <li className="item">
       <img className="item__poster" src={poster} alt={`${name} movie poster`} />
-      <div className="item__info">
-        <div>
-          <h2 className="item__info__name">{name}</h2>
-          <p className="item__info__genre">{genre}</p>
-          <p className="item__info__tech">{`${year} ${rated} ${runtime}`}</p>
+      <div className="item__info-block">
+        <div className="item__label">
+          <h2 className="item__name">{name}</h2>
+          <p className="item__genre">{genre}</p>
+          <p className="item__tech-info">{`${year} ${rated} ${runtime}`}</p>
         </div>
-        <ul className="item__info__scores">
+        <ul className="item__scores">
           <li>{scoreTomato}%</li>
           <li>{scoreTomatoUser}%</li>
           <li>{scoreImdb}</li>
           <li>{scoreMetacritic}</li>
         </ul>
-        <p>{description}</p>
-        <p>
-          Starring: {actors}
-          <br/>
-          Director: {director}
-        </p>
+        <div className="item__description">
+          <p>{description}</p>
+          <p>
+            Starring: {actors}
+            <br/>
+            Director: {director}
+          </p>
+        </div>
+        <button className="item__btn-moreinfo" onClick={onDeleteClick}>More Information</button>
         <button className="item__btn-delete" onClick={onDeleteClick}>X</button>
       </div>
     </li>
