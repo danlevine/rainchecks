fb87713040ccfd0a24306a4913068db1c722e2ee
Daniel Levine <dlevine210@gmail.com>
2016-07-23 11:59:24 -0500

    Restyle/layout Item component for responsive design and add state handling of toggleable details section for mobile

--- src/components/Item.js
+++ src/components/Item.js
@@ -1,48 +1,68 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-const Item = ({ 
-  onDeleteClick,
-  name,
-  year,
-  rated,
-  runtime,
-  genre,
-  director,
-  actors,
-  description,
-  country,
-  awards,
-  poster,
-  scoreMetacritic,
-  scoreImdb,
-  scoreTomato,
-  scoreTomatoUser,
-  tomatoConsensus }) =>
-    <li className="item">
-      <img className="item__poster" src={poster} alt={`${name} movie poster`} />
-      <div className="item__info-block">
-        <div className="item__label">
-          <h2 className="item__name">{name}</h2>
-          <p className="item__genre">{genre}</p>
-          <p className="item__tech-info">{`${year} ${rated} ${runtime}`}</p>
+class Item extends Component {
+  constructor() {
+    super();
+    this.state = {
+      itemExpanded: false
+    };
+    this.toggleExpandState = this.toggleExpandState.bind(this);
+  }
+  toggleExpandState() {
+    this.setState({itemExpanded: !this.state.itemExpanded});
+  }
+
+  render() {
+    const { onDeleteClick,
+            name,
+            year,
+            rated,
+            runtime,
+            genre,
+            director,
+            actors,
+            description,
+            country,
+            awards,
+            poster,
+            scoreMetacritic,
+            scoreImdb,
+            scoreTomato,
+            scoreTomatoUser,
+            tomatoConsensus } = this.props;
+    const expandedClassString = this.state.itemExpanded ? " item-expanded" : " item-collapsed";
+    const moreInfoBtnTxt = this.state.itemExpanded ? "Hide" : "Show";
+    return (
+      <li className={`item${expandedClassString}`}>
+        <img className="item__poster" src={poster} alt={`${name} movie poster`} />
+        <div className="item__info-block">
+          <div className="item__label">
+            <h2 className="item__name">{name}</h2>
+            <p className="item__genre">{genre}</p>
+            <p className="item__tech-info">{`${year} ${rated} ${runtime}`}</p>
+          </div>
+          <ul className="item__scores">
+            <li>{scoreTomato}%</li>
+            <li>{scoreTomatoUser}%</li>
+            <li>{scoreImdb}</li>
+            <li>{scoreMetacritic}</li>
+          </ul>
+          <div className="item__description">
+            <p className="item__plot">{description}</p>
+            <p className="item__people">
+              Starring: {actors}
+              <br/>
+              Director: {director}
+            </p>
+          </div>
+          <div className="item__footer">
+            <button className="item__btn-delete item__footer-btn" onClick={onDeleteClick}>Delete Movie</button>
+            <button className="item__btn-moreinfo item__footer-btn" onClick={this.toggleExpandState}>{`${moreInfoBtnTxt} Details`}</button>
+          </div>
         </div>
-        <ul className="item__scores">
-          <li>{scoreTomato}%</li>
-          <li>{scoreTomatoUser}%</li>
-          <li>{scoreImdb}</li>
-          <li>{scoreMetacritic}</li>
-        </ul>
-        <div className="item__description">
-          <p>{description}</p>
-          <p>
-            Starring: {actors}
-            <br/>
-            Director: {director}
-          </p>
-        </div>
-        <button className="item__btn-moreinfo" onClick={onDeleteClick}>More Information</button>
-        <button className="item__btn-delete" onClick={onDeleteClick}>X</button>
-      </div>
-    </li>
+      </li>
+    );
+  }
+}
 
 export default Item;
