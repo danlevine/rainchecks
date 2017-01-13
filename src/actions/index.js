27fccc11f2b73b73f892003ee49a135b8fa57efa
Daniel Levine <dlevine210@gmail.com>
2017-01-12 23:48:06 -0600

    Actions index.js linting fixes

--- src/actions/index.js
+++ src/actions/index.js
@@ -16,30 +16,27 @@ const db = firebase.database();
 const dbItems = db.ref('items');
 
 export const initializeItemsList = () => (dispatch) => {
-
   dispatch({
-    type: 'FETCH_ITEMS_REQUEST'
+    type: 'FETCH_ITEMS_REQUEST',
   });
 
   dbItems.on('value', snapshot => {
     // convert movie list object into array for processing
     const movieArray = _.values(snapshot.val());
     const sortedMovieArray = movieArray.sort((a, b) => moment(b.dateAdded) - moment(a.dateAdded));
-    console.log('dbitems:value');
     dispatch({
       type: 'FETCH_ITEMS_SUCCESS',
-      payload: sortedMovieArray
+      payload: sortedMovieArray,
     });
   });
 
   dbItems.on('child_added', snapshot => {
-
     const currentItem = snapshot.val();
 
     // Copy item key into object for convenience when iterating through items
     if (!currentItem.key) {
       db.ref('items/' + snapshot.key).update({
-        key: snapshot.key
+        key: snapshot.key,
       });
     }
     if (!currentItem.lastFetched) {
@@ -50,9 +47,9 @@ export const initializeItemsList = () => (dispatch) => {
         var omdbEndpoint = 'https://www.omdbapi.com/?t=' + currentItem.name.replace(/\s/g, '+') + '&y=&plot=short&r=json&type=movie&tomatoes=true';
       }
 
-      let currentDateTime = moment().format();
-      axios(omdbEndpoint).then(({data}) => {
-        let appendedProps = {
+      const currentDateTime = moment().format();
+      axios(omdbEndpoint).then(({ data }) => {
+        const appendedProps = {
           status: 'active',
           lastFetched: currentDateTime,
           dateAdded: currentDateTime,
@@ -72,63 +69,61 @@ export const initializeItemsList = () => (dispatch) => {
           scoreTomato: data.tomatoMeter,
           scoreTomatoUser: data.tomatoUserMeter,
           tomatoConsensus: data.tomatoConsensus,
-          imdbID: data.imdbID
+          imdbID: data.imdbID,
         };
         db.ref('items/' + snapshot.key).update(appendedProps);
-        return;
       }).catch(error => {
         // TODO: add error handling mechanism
         console.log(error); // eslint-disable-line no-console
       });
     }
-
   });
 };
 
-export const addItem = (item) => (dispatch) => {
+export const addItem = item => (dispatch) => {
   if (typeof item === 'object') {
-    dbItems.push({name: item.Title, imdbID: item.imdbID});
+    dbItems.push({ name: item.Title, imdbID: item.imdbID });
   }
   else {
-    dbItems.push({name: item.Title});
+    dbItems.push({ name: item.Title });
   }
-}
+};
 
-export const archiveItem = (key) => (dispatch) =>
+export const archiveItem = key => dispatch =>
   dbItems.child(key).update({ status: 'archived' });
 
-export const unarchiveItem = (key) => (dispatch) =>
+export const unarchiveItem = key => dispatch =>
   dbItems.child(key).update({ status: 'active' });
 
-export const deleteItem = (key) => (dispatch) => 
+export const deleteItem = key => dispatch =>
   dbItems.child(key).remove();
 
-export const loadSuggestions = (value) => (dispatch) => {
+export const loadSuggestions = value => (dispatch) => {
   dispatch(loadSuggestionsBegin());
 
-  let omdbEndpoint = 'https://www.omdbapi.com/?s=' + value + '&type=movie';
-  
-  axios(omdbEndpoint).then(({data}) => {
+  const omdbEndpoint = 'https://www.omdbapi.com/?s=' + value + '&type=movie';
+
+  axios(omdbEndpoint).then(({ data }) => {
     dispatch(maybeUpdateSuggestions(data.Search || [], value));
   });
 };
 
-export const updateInputValue = (value) => (dispatch) => {
+export const updateInputValue = value => (dispatch) => {
   dispatch({
     type: 'UPDATE_INPUT_VALUE',
-    value
+    value,
   });
 };
 
 export const clearSuggestions = () => (dispatch) => {
   dispatch({
-    type: 'CLEAR_SUGGESTIONS'
+    type: 'CLEAR_SUGGESTIONS',
   });
 };
 
 export const loadSuggestionsBegin = () => (dispatch) => {
   dispatch({
-    type: 'LOAD_SUGGESTIONS_BEGIN'
+    type: 'LOAD_SUGGESTIONS_BEGIN',
   });
 };
 
@@ -136,31 +131,31 @@ export const maybeUpdateSuggestions = (suggestions, value) => (dispatch) => {
   dispatch({
     type: 'MAYBE_UPDATE_SUGGESTIONS',
     suggestions,
-    value
+    value,
   });
 };
 
-export const activateAddItem = () => (dispatch) =>
+export const activateAddItem = () => dispatch =>
   dispatch({
-    type: 'ADD_ITEM_FORM_ACTIVATE'
+    type: 'ADD_ITEM_FORM_ACTIVATE',
   });
 
-export const cancelAddItem = () => (dispatch) =>
+export const cancelAddItem = () => dispatch =>
   dispatch({
-    type: 'ADD_ITEM_FORM_CANCEL'
+    type: 'ADD_ITEM_FORM_CANCEL',
   });
 
-export const showActiveList = () => (dispatch) =>
+export const showActiveList = () => dispatch =>
   dispatch({
-    type: 'FILTER_DISPLAY_ACTIVE'
+    type: 'FILTER_DISPLAY_ACTIVE',
   });
 
-export const showArchivedList = () => (dispatch) =>
+export const showArchivedList = () => dispatch =>
   dispatch({
-    type: 'FILTER_DISPLAY_ARCHIVED'
+    type: 'FILTER_DISPLAY_ARCHIVED',
   });
 
-export const showFullList = () => (dispatch) =>
+export const showFullList = () => dispatch =>
   dispatch({
-    type: 'FILTER_DISPLAY_ALL'
+    type: 'FILTER_DISPLAY_ALL',
   });
