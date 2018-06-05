aee8564e0f435d9d29ba47a77406d6f69ef81815
Daniel Levine <dlevine210@gmail.com>
2018-06-05 03:26:31 -0500

    Only display first 6 items by default and Show More button to display the rest

--- src/components/ItemList.js
+++ src/components/ItemList.js
@@ -1,22 +1,66 @@
 import _ from 'lodash';
-import React from 'react';
+import React, { Component } from 'react';
 import Item from './Item';
 
 
-const ItemList = ({ items, onArchiveClick, onUnarchiveClick, onDeleteClick }) => {
-  return (
-    <ul className="item-list">
-      {_.map(items.filteredItems, (item) =>
-        <Item 
-          key={item.key}
-          {...item}
-          onArchiveClick={() => onArchiveClick(item.key)}
-          onUnarchiveClick={() => onUnarchiveClick(item.key)}
-          onDeleteClick={() => onDeleteClick(item.key)}
-        />
-      )}
-    </ul>
-  );
-};
+class ItemList extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      moreItemsVisible: false,
+    };
+
+    this.showMoreItems = this.showMoreItems.bind(this);
+  }
+
+  createItem(item) {
+    const { onArchiveClick, onUnarchiveClick, onDeleteClick } = this.props;
+
+    return (
+      <Item
+        key={item.key}
+        {...item}
+        onArchiveClick={() => onArchiveClick(item.key)}
+        onUnarchiveClick={() => onUnarchiveClick(item.key)}
+        onDeleteClick={() => onDeleteClick(item.key)}
+      />
+    );
+  }
+
+  showMoreItems() {
+    this.setState({ moreItemsVisible: true });
+  }
+
+  render() {
+    const { items } = this.props;
+    const { moreItemsVisible } = this.state;
+
+    return (
+      <ul className="item-list">
+        {_.map(items.filteredItems.slice(0, 6), item =>
+          this.createItem(item)
+        )}
+
+        { !moreItemsVisible &&
+          <li>
+            <button
+              className="item-list__show-more-btn"
+              onClick={this.showMoreItems}
+            >
+              Show More
+            </button>
+          </li>
+        }
+
+        { moreItemsVisible &&
+          _.map(items.filteredItems.slice(6, items.filteredItems.length), item =>
+            this.createItem(item)
+          )
+        }
+      </ul>
+    );
+  }
+}
 
 export default ItemList;
