219e7104a6c387dbb98033d2ea5130ae2dd8ddde
Daniel Levine <dlevine210@gmail.com>
2018-10-03 13:56:32 -0500

    Adds Google Authenication functionality

--- src/components/ItemsContainer.js
+++ src/components/ItemsContainer.js
@@ -1,36 +1,98 @@
-import React, { Component } from 'react';
-import { connect } from 'react-redux';
+import React, { Component } from "react";
+import { connect } from "react-redux";
+import firebase from "firebase";
 
-import * as actions from '../actions';
-import ItemList from '../components/ItemList';
+import * as actions from "../actions";
+import ItemList from "../components/ItemList";
+
+const provider = new firebase.auth.GoogleAuthProvider();
+const auth = firebase.auth();
 
 class ItemsContainer extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      username: "",
+      user: null
+    };
+
+    this.login = this.login.bind(this);
+    this.logout = this.logout.bind(this);
+  }
 
   componentDidMount() {
+    // If user was previously logged in, auto log-in
+    auth.onAuthStateChanged(user => {
+      if (user) {
+        this.setState({ user });
+      }
+    });
+
     this.props.initializeItemsList();
   }
 
+  login() {
+    auth.signInWithPopup(provider).then(result => {
+      const user = result.user;
+      this.setState({
+        user
+      });
+    });
+  }
+
+  logout() {
+    auth.signOut().then(() => {
+      this.setState({
+        user: null
+      });
+    });
+  }
+
   render() {
-    const { archiveItem, unarchiveItem, deleteItem, items, isFetching } = this.props;
-    if (isFetching) {
+    const {
+      archiveItem,
+      unarchiveItem,
+      deleteItem,
+      items,
+      isFetching
+    } = this.props;
+
+    if (this.state.user) {
+      // logged in
       return (
-        <div className="spinner spinner-dark">
-          <span className="spinner__text">Loading...</span>
+        <div>
+          <button onClick={this.logout}>Log Out</button>
+          {isFetching ? (
+            <div className="spinner spinner-dark">
+              <span className="spinner__text">Loading...</span>
+            </div>
+          ) : (
+            <ItemList
+              items={items}
+              onArchiveClick={archiveItem}
+              onUnarchiveClick={unarchiveItem}
+              onDeleteClick={deleteItem}
+            />
+          )}
+        </div>
+      );
+    } else {
+      // NOT logged in
+      return (
+        <div>
+          <h3>
+            Log in with your Google account to add sweet flicks for those rainy
+            days!
+          </h3>
+          <button onClick={this.login}>Log In</button>
         </div>
       );
     }
-    return (
-      <ItemList
-        items={items}
-        onArchiveClick={archiveItem}
-        onUnarchiveClick={unarchiveItem}
-        onDeleteClick={deleteItem} 
-      />
-    );
-  };
+  }
 }
 
-const mapStateToProps = ({items, isFetching}) => {
+const mapStateToProps = ({ items, isFetching }) => {
   return {
     items,
     isFetching
